a0869765440bdae035567d85888ad14388fa8fb5
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-12-11 23:32:00 +0530

    authorize , save cred , load cred [Controller works] but list not give me the response

--- Backend/controller/Drive.controller.js
+++ Backend/controller/Drive.controller.js
@@ -5,103 +5,102 @@ const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
 const { message } = require("statuses");
 const { error } = require("console");
-
+const { response } = require("express");
+const { oauth2 } = require("googleapis/build/src/apis/oauth2");
 // If modifying these scopes, delete token.json.
 const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
+
 const CREDENTIALS_PATH = path.join(process.cwd(), "secre.json");
 const TOKEN_PATH = path.join(process.cwd(), "token.json");
-/**
- * Reads previously authorized credentials from the save file.
- *
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
+
+async function loadSavedCredentialsIfExist(req, res) {
   try {
     const content = await fs.readFile(TOKEN_PATH);
     const credentials = JSON.parse(content);
+    res.json({
+      message: "save credientials if exist ",
+      data: credentials,
+    });
     return google.auth.fromJSON(credentials);
   } catch (err) {
     return null;
   }
 }
-
-/**
- * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
+async function saveCredentials(client , res) {
   const content = await fs.readFile(CREDENTIALS_PATH);
   const keys = JSON.parse(content);
   console.log("key");
   console.log(keys);
   const key = keys.installed || keys.web;
-  try{
+  try {
     const payload = JSON.stringify({
       type: "authorized_user",
       client_id: key.client_id,
       client_secret: key.client_secret,
     });
-    console.log(payload)
     await fs.writeFile(TOKEN_PATH, payload);
-  }catch(er){
+    return res.json({
+      message: "true",
+      data: payload,
+    });
+  } catch (er) {
     console.log(er);
-    
   }
-  
-
 }
-
-/**
- * Load or request or authorization to call APIs.
- *
- */
 async function authorize() {
   let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-
-    
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-    res.json({
-      message: "authorized succesfully",
-      success: true,
+  try {
+    if (client) {
+      console.log("authorized client")
+      return client;
+    }
+    client = await authenticate({
+      scopes: SCOPES,
+      keyfilePath: CREDENTIALS_PATH,
     });
+    if (client.credentials) {
+      await saveCredentials(client);
+      console.log("client credietinals done")
+    }
+
+    return client;
+  } catch (er) {
+    console.log(er);
   }
-  return client;
 }
 
+async function listFiles(oauth2 , res) {
+  const drive = google.drive({ version: "v3", auth: oauth2 });
+  console.log("gogogle drive resp");
+  console.log(drive);
 
-async function listFiles(authClient) {
-  const drive = google.drive({ version: "v3", auth: authClient });
-  console.log(drive)
-  const res = await drive.files.list({
-    pageSize: 1,
-    fields: "nextPageToken, files(id, name)",
-  });
-  console.log(res.data.files);
+  try {
+    const response = await drive.files.list({
+      pageSize: 1, // Set the desired number of files to retrieve
+      fields: "files(name, id)", // Specify the fields to include in the response
+    });
+    const files = response.data.files;
+    console.log("files of the data");
+    console.log(files);
 
-  const files = res.data.files;
-  if (files.length === 0) {
-    console.log("No files found.");
-    return;
+    // const files = res.data.files;
+    if (files.length === 0) {
+      console.log("No files found.");
+      return;
+    }
+    console.log("Files:");
+   let FileFilteredData =  files.map((file) => {
+      console.log(`${file.name} (${file.id})`);
+    });
+
+    res.json({
+      message:"true",
+      data:FileFilteredData
+    })
+
+  } catch (err) {
+    console.error("Error listing files:", err);
   }
-  console.log("Files:");
-  files.map((file) => {
-    console.log(`${file.name} (${file.id})`);
-    
-  });
 }
 
 module.exports = {
