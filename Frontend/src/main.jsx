7125134736d0b38a00ae6e7c3bdd171b1f6c5371
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-05-12 23:59:40 +0530

    fix:build issues

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "@radix-ui/themes/styles.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { UserProvider } from "./context/UserContext.jsx";
